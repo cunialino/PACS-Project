7cac7f4ab61a7d46931d3ba36aa5fa2ada1427ba
cunialino <elia.cunial@gmail.com>
2020-06-08 15:04:26 +0200

    Update readme, added install.sh (first version)

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Install pytorch 
+
+cd libs
+
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+
+#unzip libtorch-shared-with-deps-latest.zip
+
